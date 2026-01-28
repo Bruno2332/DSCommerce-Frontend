e31e0509a96e102c74de89337c5470c5c2390e98
Bruno Flor de Lys <brunolys23@gmail.com>
2026-01-28 16:43:13 -0300

    Error CSS

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -53,7 +53,9 @@ export default function ProductForm() {
     function handleInputChange(event: any) {
             const value = event.target.value;
             const name = event.target.name;
-            setFormData(forms.update(formData, name, value))
+            const dataUpdated = forms.update(formData, name, value);
+            const dataValidated = forms.validate(dataUpdated, name)
+            setFormData(dataValidated);
         }
 
     return (
@@ -68,6 +70,7 @@ export default function ProductForm() {
                                 className="form-controls"
                                 onChange={handleInputChange}
                             />
+                            <div className='form-error'>{formData.name.message}</div>
                         </div>
                         <div>
                             <FormInput
@@ -75,6 +78,7 @@ export default function ProductForm() {
                                 className="form-controls"
                                 onChange={handleInputChange}
                             />
+                            <div className='form-error'>{formData.price.message}</div>
                         </div>
                         <div>
                             <FormInput
